16f5708a8db24d6b1d4d33dcdd5b1880e83259ec
owarin <mateusz.nawiesniak@gmail.com>
2022-05-11 10:56:53 +0200

    Added first relationship

---
+++ meidoCafe/Models/Position.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace meidoCafe.Models
+{
+    internal class Position
+    {
+        public int PositionId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Pay { get; set; }
+    }
+}

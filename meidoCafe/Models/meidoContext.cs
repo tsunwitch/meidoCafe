02a0884a902504a8c9850e7656bf4222fe00caab
owarin <mateusz.nawiesniak@gmail.com>
2022-05-11 09:58:34 +0200

    Created database context

---
+++ meidoCafe/Models/meidoContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace meidoCafe.Models
+{
+    internal class meidoContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=meidoDB;Trusted_Connection=true");
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Customre> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+    }
+}

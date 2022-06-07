c2c82fdd7eb21401a828b8a58f0ad2d33cf6c817
owarin <mateusz.nawiesniak@gmail.com>
2022-06-07 11:54:29 +0200

    Added remove button and reading product names from list

--- meidoCafe/MVVM/View/ConfigurationView.xaml.cs
+++ meidoCafe/MVVM/View/ConfigurationView.xaml.cs
@@ -30,6 +30,8 @@ namespace meidoCafe.MVVM.View
             using (var ctx = new MeidoContext())
             {
                 var ProdList = new ObservableCollection<Product>(ctx.Products);
+
+                PRemoveComboBox.ItemsSource = ProdList;
             }
         }
     }
